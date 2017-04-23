0bd120d2383d6311941acc0f11c6ea356b34cf70
Lindsay Maher <lindsaymah3r@gmail.com>
2017-04-22 20:33:05 -0500

    add basic applicant questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,24 @@
+#Find out the applicants name
+
+puts "What is your name?"
+applicant_name = gets.chomp
+
+#Find out applicants age
+
+puts "How old are you?"
+applicant_age = gets.chomp
+
+#Follow up with asking year of birth
+
+puts "Awesome, so what year were you born?"
+applicant_birth_year = gets.chomp
+
+#Ask whether they'd like garlic bread
+
+puts "Our cafeteria serves garlic bread, should we order you some? (yes/no)"
+garlicbread_preference = gets.chomp
+
+#Health insurance?
+
+puts "Would you like to enroll in the company’s health insurance?"
+health_insurance = gets.chomp
