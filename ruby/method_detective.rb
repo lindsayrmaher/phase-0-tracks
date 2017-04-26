24abeb93a1b77b439a4750f680619a38088403b4
Lindsay Maher <lindsaymah3r@gmail.com>
2017-04-25 22:39:48 -0500

    did research on what different methods do to the class string

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,65 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+ investigation = "iNvEsTiGaTiOn".swapcase
+ 	puts investigation
+ 
+ investigation_2 = "iNvEsTiGaTiOn".swapcase!
+ 	puts investigation_2
+
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+second_method = "zom".insert(1, 'o') 
+	puts second_method
+
 # => “zoom”
 
-# "enhance".<???>
+third_method_1 = "enhance".center(26)
+third_method_2 = "enhance".center(6)
+third_method_3 = "enhance".center(40, "boo")
+	puts third_method_1
+	puts third_method_2
+	puts third_method_3
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+fourth_method = "Stop! You’re under arrest!".upcase
+	puts fourth_method
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+fifth_method =  "the usual ".concat("suspects")
+	puts fifth_method
 #=> "the usual suspects"
 
-# " suspects".<???>
+a = " suspects"
+sixth_method = a.prepend("the usual")
+	puts sixth_method
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+seventh_method = "The case of the disappearing last letter".chop
+	puts seventh_method
+
+seventh_method_2 = "The case of the disappearing last letter".chop!
+
+	puts seventh_method_2
+
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+eigth_method = "The mystery of the missing first letter".delete "T"
+	puts eigth_method
+
+eigth_method_2 = "The mystery of the missing first letter".tr('T', '')
+	puts eigth_method_2
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+ninth_method = "Elementary,    my   dear        Watson!".squeeze
+	puts ninth_method
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+tenth_method =  "z".each_byte {|c| print c, ' ' }
+	puts tenth_method
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+eleventh_method = "How many times does the letter 'a' appear in this string?".count "a"
+	puts eleventh_method
 # => 4
