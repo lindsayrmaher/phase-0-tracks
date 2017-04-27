2f1b38e70a0df0179e2aa5629f0c261f8e4715e7
Lindsay Maher <lindsaymah3r@gmail.com>
2017-04-27 07:46:54 -0500

    add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,47 @@
+#Create a new hash, leave empty for now
+
+designer_response = {
+
+}
+
+#Collect the Details from the Interior Designer
+
+#Ask for the Client's Name
+#give the answer set to the key in the hash
+
+#print hash to test if key has been added
+
+#Ask for their age
+#give answer set to key in hash
+
+#print hash to test if key has been added
+
+#Ask what kind of decor theme they are looking for
+#answer set to key in hash
+
+#print hash to test if key has been added
+
+#Ask if they have expensive taste
+#answer set to key in hash
+
+#print hash to test if key has been added
+
+#Ask how many months they think the project will take
+#answer set to key in hash
+
+#print hash to test if key has been added
+
+#remove test prints
+
+#print the hash values once they've completed the form
+
+#Ask if there are any fields they need to update
+#ask them to either say "none" if they are done, or say which field name they need to update
+#set their response equal to their previous response changed to a symbol
+
+#print the new set of values in the hash to make sure they key has been updated
+
+#print a thank you for filling out the form!
+
+
+
