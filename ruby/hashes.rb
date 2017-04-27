73d1eea3d7f6eb45cb5e4b5a196e6430c6bc0017
Lindsay Maher <lindsaymah3r@gmail.com>
2017-04-27 14:28:27 -0500

    complete initial response section and add first step of field update

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -13,7 +13,6 @@ puts "What is your name?"
 designer_response[:client_name] = gets.chomp
 
 #print hash to test if key has been added
-puts designer_response.values
 
 #Ask for their age
 #give answer set to key in hash
@@ -22,7 +21,6 @@ puts "How old are you?"
 designer_response[:client_age] = gets.chomp.to_i
 
 #print hash to test if key has been added
-puts designer_response.values
 
 #Ask what kind of decor theme they are looking for
 #answer set to key in hash
@@ -32,10 +30,16 @@ designer_response[:decor_theme] = gets.chomp
 
 #print hash to test if key has been added
 
-puts designer_response.values
-
 #Ask if they have expensive taste
 #answer set to key in hash
+
+puts "Do you have expensive taste? (y/n)"
+designer_response[:taste] = gets.chomp
+	if designer_response[:taste] == "y" or designer_response[:taste]
+		designer_response[:taste] = true
+	else
+		designer_response[:taste] = false
+	end
 
 #print hash to test if key has been added
 
@@ -47,14 +51,18 @@ designer_response[:project_length] = gets.chomp.to_i
 
 #print hash to test if key has been added
 
-puts designer_response
-
 #remove test prints
 
 #print the hash values once they've completed the form
 
+puts "Inputted Values"
+puts designer_response.values
+
 #Ask if there are any fields they need to update
 #ask them to either say "none" if they are done, or say which field name they need to update
+
+puts "Before you submit, are there any fields you need to update?"
+puts "If not, go ahead and say 'none', if yes please type the field you want to update"
 #set their response equal to their previous response changed to a symbol
 
 #print the new set of values in the hash to make sure they key has been updated
