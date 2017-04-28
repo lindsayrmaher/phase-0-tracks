0e6a69b9d0e8d0f4b4032b1a3df1ece744d80570
Lindsay Maher <lindsaymah3r@gmail.com>
2017-04-27 20:54:17 -0500

    made an array and hash, and used .each and .map! to run through and modify

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,36 @@
-def robot(arms, legs)
-	limbs = arms + legs
-	puts limbs
-	yield
+#release zero
+#def robot(arms, legs)
+#	limbs = arms + legs
+#	puts limbs
+#	yield
+#end
+#robot(12,24) { puts "That's a lot of limbs!"}
+
+
+#release one
+
+age_of_robots = [1,25,13,43,5,610]
+
+robot_features = {
+	eyes: 2,
+	mouth: 3,
+	arms: 12,
+	legs: 24,
+	buttons: 100
+
+}
+
+age_of_robots.each do |age|
+	puts "This robot is #{age} today!"
 end
-robot(12,24) { puts "That's a lot of limbs!"}
+
+robot_features.each do |feature, number|
+	puts "This robot has this #{number} #{feature}!"
+end
+
+age_of_robots.map! do |age|
+	puts age
+	age.next
+end
+
+p age_of_robots
