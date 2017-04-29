c13bbfa4895f0a2d9ec3e62348b7ff4066c17209
Lindsay Maher <lindsaymah3r@gmail.com>
2017-04-29 18:32:43 -0500

    add almost all of steps for release 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -29,3 +29,38 @@ index = 0
 end
 
 p search_party(sample_array, 37)
+
+#release 1
+
+#create iterative sequence to add together 2 previous index points 
+# find method to move forward one index point with each loop
+# for each new number in array, add previous number to it
+#store the result as the next item in the array
+
+#define method
+
+def fibonacci(n)
+  return  n  if n <= 1 
+    fibonacci( n - 1 ) + fibonacci( n - 2 )
+  end 
+
+puts fibonacci(5)
+
+def fib_sequence(n)
+
+  range = 0..n.to_a
+
+    while n > fibonacci_array.length do 
+
+      fibonacci(n)
+
+      fibonacci_array << fibonacci(n)
+
+    end
+
+ p fibonacci_array
+
+end
+
+
+fib_sequence(5)
