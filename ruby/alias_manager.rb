f599691e44d38993d0d5d9c2f25a3e20c3dddaa3
Lindsay Maher <lindsaymah3r@gmail.com>
2017-04-29 09:21:23 -0500

    add pseudocode and first line of program

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,29 @@
+#Create a Alias Generator
+
+#Input Real Name as string
+
+real_name = "Lindsay Maher"
+
+#make all letters of string lowercase
+
+#Convert the string to an array
+
+#Reverse the order of the array inputs
+
+#split by white space
+
+#Iterate through each word to find vowels & consonants
+#if it is a vowel, rotate/replace to/with the next vowel in the alphabet
+#if it is a consonant, rotate/replace to/with next consonant in the alphabet
+#deal with edge cases here - if a z, replace with !
+
+#make sure to either run with the boom modifier or create a new variable to equal the new array
+
+#change the array back to a string
+
+#capitalize the first letter of each word
+
+#join the two back togehter as one string
+
+#print the result
+
