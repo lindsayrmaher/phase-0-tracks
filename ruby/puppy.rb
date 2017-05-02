015b172984296f54dda4c7b2d733293f6d25a4ad
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-02 08:59:15 -0500

    comment out puppy design

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,25 +1,25 @@
-SPECIES ------------------------
-Pupperitis Cutosis
+# SPECIES ------------------------
+# Pupperitis Cutosis
 
-CHARACTERISTICS ----------------
-Eye Count: 2
-Lovable: yes
-Playful: yes
-Fur Type: Fuzzy
-Name: Varies
-Fur Color: Varies
-Teeth Sharpness Level: Low/Medium
+# CHARACTERISTICS ----------------
+# Eye Count: 2
+# Lovable: yes
+# Playful: yes
+# Fur Type: Fuzzy
+# Name: Varies
+# Fur Color: Varies
+# Teeth Sharpness Level: Low/Medium
 
-BEHAVIOR ----------------------
-Jump
-Run
-Bark
-Play
-Growl
-Bite
-Cuddle
-Eat
-Make Messes
-Captures Hearts
-Eats Shoes
-Uses house as personal bathroom
+# BEHAVIOR ----------------------
+# Jump
+# Run
+# Bark
+# Play
+# Growl
+# Bite
+# Cuddle
+# Eat
+# Make Messes
+# Captures Hearts
+# Eats Shoes
+# Uses house as personal bathroom
