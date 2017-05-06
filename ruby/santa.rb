4cba6f4c799ab59464ace470f60569b482f150fd
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-05 21:56:06 -0500

    test sampling from arrays and set up a random age from a range of numbers for santa creator

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
-	attr_accessor :gender
-	attr_reader :age, :ethnicity
+	attr_accessor :gender, :age
+	attr_reader :ethnicity
 
 	def initialize(gender, ethnicity)
 
@@ -53,8 +53,32 @@ end
 
 # santa_crew = []
 
-# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+def create_instance(number_of_instances)
+	example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+	example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+	number_of_instances.times do 
+		number_of_instances.to_s = Santa.new(example_genders.sample, example_ethnicities.sample)
+		
+		updated_age = Random.new
+		new_santa.age = updated_age.rand(0..140)
+		p new_santa.age
+
+	end
+end
+
+create_instance(50)
+
+# new_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+
+# updated_age = Random.new
+# new_santa.age = updated_age.rand(0..140)
+
+# p new_santa.age
+
+
+
+
 # example_genders.length.times do |i|
 #   santa_crew << Santa.new(example_genders[i], example_ethnicities[i])
 # end
@@ -64,11 +88,11 @@ end
 # santa_crew << Santa.new("transgender", "Australian")
 
 
-santa_four = Santa.new("gender fluid", "Japanese-African")
-puts "The santa's age is #{santa_four.age}"
-santa_four.celebrate_birthday
-puts "This santa is now #{santa_four.age}!"
-santa_four.gender = "nothing"
-puts "santa's gender is now #{santa_four.gender}"
-santa_four.get_mad_at("Rudolph")
+# santa_four = Santa.new("gender fluid", "Japanese-African")
+# puts "The santa's age is #{santa_four.age}"
+# santa_four.celebrate_birthday
+# puts "This santa is now #{santa_four.age}!"
+# santa_four.gender = "nothing"
+# puts "santa's gender is now #{santa_four.gender}"
+# santa_four.get_mad_at("Rudolph")
 
