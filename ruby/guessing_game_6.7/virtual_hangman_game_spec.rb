2d9aa157983ad5c80ce8f63e99ecbf43a356a3fc
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-07 20:20:53 -0500

    update first test for first method

--- ruby/guessing_game_6.7/virtual_hangman_game_spec.rb
+++ ruby/guessing_game_6.7/virtual_hangman_game_spec.rb
@@ -3,12 +3,10 @@
 require_relative 'virtual_hangman_game'
 
 describe Guessing_Game do 
-	 # let(:user_input) { Guessing_Game.new }
+	 let(:user_input) { Guessing_Game.new }
 
 	 it "changes the the word submitted into a disguised string" do
-	 	user_input = Guessing_Game.new
-	 	user_input.disguise_word("unicorn")
-	 	expect(user_input.disguise_word).to start_with '__'
+	 	expect(user_input.disguise_word("unicorn")).to start_with '__'
 	 end
 
 
