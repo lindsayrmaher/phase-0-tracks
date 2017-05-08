898945afbceadfa6438a0f4ed4b6255e65e7dc32
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-07 19:08:13 -0500

    add first expectation to rspec

--- ruby/guessing_game_6.7/virtual_hangman_game_spec.rb
+++ ruby/guessing_game_6.7/virtual_hangman_game_spec.rb
@@ -3,6 +3,13 @@
 require_relative 'virtual_hangman_game'
 
 describe Guessing_Game do 
+	 let(:user_input) { Guessing_Game.new }
+
+	 it "changes the the word submitted into a disguised string" do
+	 	expect(user_input.disguise_word).to include ('__')
+	 end
+
+
 	
 end
 
