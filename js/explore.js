f2b65a4d85e2de8e09af9cfa010220f37445b110
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-12 12:28:43 -0500

    wrote pseudocode, create funcion container

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// In comments, pseudocode a function that takes a string as a parameter and reverses the string.
+// Need a string variable to work with - the parameter
+// Create a loop that will go through each letter of the string
+// Want to start at the end of the string and work backwards
+// Maybe make a new string to add the letters to as it loops (somewhere to store the characters of the inputted string)
+// return the new variable in the function so that we can print it when we run the function
+
+function reverseString(inputString) {
+	var newString = "";
+}
