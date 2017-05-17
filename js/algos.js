c3af024f46fd9b04094fbc3bde50582b15742cf1
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-17 06:52:19 -0500

    add pseudocode for first algorithm

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// Practicing Javascript Algorithms
+
+// First Drill
+// Return the longest word of the array
+
+// Want to run through each word in the array and compare lengths
+// IF the word length is longer than the word after it, move it to the end (index -1)
+// OR IF the word is shorter than the word before it, move it in front of that word
+// OTHERWISE keep it in the same position
+// Once all the words have been sorted, return the last word of the array (should be longest)
