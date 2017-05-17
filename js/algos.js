3d9ed60e2c629223f6173a36bba5d6b82ea52e50
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-17 07:09:23 -0500

    sorting algorithm created

--- js/algos.js
+++ js/algos.js
@@ -8,3 +8,15 @@
 // OR IF the word is shorter than the word before it, move it in front of that word
 // OTHERWISE keep it in the same position
 // Once all the words have been sorted, return the last word of the array (should be longest)
+function bubbleSort(word_array) {
+	var i;
+	var n;
+		while(word_array.length--) {
+			for(i = 0, n = 1; i < word_array.length; ++i, ++n)
+				if(word_array[i].length > word_array[n].length) 
+					{place_holder = word_array[i]; word_array[i]=word_array[n]; word_array[n] = place_holder;}
+
+		}
+		return word_array[-1];
+}
+
