c5f4dddf6acdd3900a8211a8a256aaa5b026ba28
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-22 07:50:59 -0500

    add pizza to table and retrieve info about it

--- databases/solo_project_2/pizza_maker.rb
+++ databases/solo_project_2/pizza_maker.rb
@@ -6,3 +6,26 @@ require 'faker'
 # create SQLite3 database
 db = SQLite3::Database.new("pizzas.db")
 db.results_as_hash = true
+
+#making initial pizza table
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS pizza(
+    id INTEGER PRIMARY KEY,
+    pizza_name VARCHAR(255),
+    num_toppings INT,
+    crisp_crust BOOLEAN,
+    size INT
+  )
+SQL
+
+#make pizza table
+db.execute(create_table_cmd)
+
+#make first pizza!!
+db.execute("INSERT INTO pizza (pizza_name, num_toppings, crisp_crust, size) VALUES ('Margarita', 3, 'true', 12)")
+
+#retrieve za info
+pizza = db.execute("SELECT * FROM pizza")
+pizza.each do |pizza|
+ puts "Pizza Order: A #{pizza['size']}in #{pizza['pizza_name']} with #{pizza['num_toppings']} toppings!"
+end
