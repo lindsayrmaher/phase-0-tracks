0ac8c78e16c59e5a6bcce309dfc87fefc6eef02a
Lindsay Maher <lindsaymah3r@gmail.com>
2017-05-24 21:29:13 -0500

    add JS manipulation to page

---
+++ web_dev/client_side_js/week_nine_script.js
@@ -0,0 +1,23 @@
+var main_heading = document.getElementById('man_bun');
+
+var sub_heading = document.createElement('h2');
+
+var h2_text = document.createTextNode('Welcome to Hipster Universe')
+
+sub_heading.appendChild(h2_text);
+// console.log(main_heading);
+
+main_heading.style.fontFamily = 'Helvetica';
+
+var list = document.getElementsByTagName('ul');
+
+list[0].style.backgroundColor = '#ddbd06';
+
+
+// insert h2 heading 
+
+var parent = main_heading.parentNode;
+
+parent.insertBefore(sub_heading, main_heading);
+
+// main_heading.appendChild(sub_heading);
