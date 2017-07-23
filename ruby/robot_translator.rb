49056a7228599db45cfbc1d160cc63a59781af62
Lindsay Maher <lindsaymah3r@gmail.com>
2017-07-22 19:38:01 -0500

    created file and added pseudocode

---
+++ ruby/robot_translator.rb
@@ -0,0 +1,12 @@
+#Robot Translator
+
+#if letter is capitalized and is in the first half of the alphabet, it become "bloop"
+
+#otherwise if a letter is capitalized and is the letter E, it becomes "buzz"
+
+#If it's not a letter at all it becomes "boing"
+
+#otherwise it becomes "beep"
+
+#"Happy halloween!" => "bloopbeepbeepbeepbeepboingbloopbeepbeepbeepbeepbeepbuzzbuzzbeepboing"
+
