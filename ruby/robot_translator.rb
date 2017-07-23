4f41306515d3f3bda35fd804984f1c7e9c74eb1d
Lindsay Maher <lindsaymah3r@gmail.com>
2017-07-22 19:44:30 -0500

    create translate_phrase method

--- ruby/robot_translator.rb
+++ ruby/robot_translator.rb
@@ -10,3 +10,12 @@
 
 #"Happy halloween!" => "bloopbeepbeepbeepbeepboingbloopbeepbeepbeepbeepbeepbuzzbuzzbeepboing"
 
+def translate_phrase(phrase)
+	char_index = 0
+	while char_index < phrase.length
+		puts phrase[char_index]
+		char_index += 1
+	end
+end
+
+translate_phrase("Happy Halloween!")
